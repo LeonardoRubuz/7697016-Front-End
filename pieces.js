449efc42ed91c07600f8fb0f013df5f041b8946f
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-08-01 04:26:01 +0100

    elements attached to the document in the right section

--- pieces.js
+++ pieces.js
@@ -6,12 +6,26 @@ const pieces = await reponse.json();
 const article = pieces[0];
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
+imageElement.alt = "Une image";
 
 const nomElement = document.createElement('h2');
 nomElement.innerText = article.nom;
 
 const prixElement = document.createElement('p');
-prixElement.innerText  = `Prix: ${article.prix}$`;
+prixElement.innerText  = `Prix: ${article.prix}€`;
 
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie;
+
+// Rattachement des éléments dans le document
+
+const sectionFiches = document.querySelector(".fiches");
+let elementsList = [
+    imageElement,
+    nomElement,
+    prixElement,
+    categorieElement
+]
+for (let element of elementsList) {
+    sectionFiches.appendChild(element);
+}
